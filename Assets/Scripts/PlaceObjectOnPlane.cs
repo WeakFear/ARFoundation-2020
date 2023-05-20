537b242e52f14ae24cb590262125e70860308493
Arisa Alcantara <arisa.alcantara@iacademy.edu.ph>
2023-05-20 13:56:51 +0800

    Added place object on plane

--- Assets/Scripts/PlaceObjectOnPlane.cs
+++ Assets/Scripts/PlaceObjectOnPlane.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.ARFoundation;
 
+// Add this header if you want your component to be added automatically
+[RequireComponent(typeof(ARRaycastManager))]
 public class PlaceObjectOnPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // What object would you like to spawn
+    [SerializeField] private GameObject prefab;
+    private GameObject spawnedObject;
+
+    private ARRaycastManager raycaster;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+    private void Start()
     {
-        
+        raycaster = GetComponent<ARRaycastManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPlaceObject(InputValue value)
     {
-        
+        // Get the screen touch position
+        Vector2 touchPosition = value.Get<Vector2>();   
+        // Perform a raycast from the touchPosition into the 3D scene to look for a plane
+        if(raycaster.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.PlaneWithinPolygon))
+        {
+            // Get the hit point (pose) on the plane
+            Pose hitPoint = hits[0].pose;
+            // Is this the first time we will place an object?
+            if(spawnedObject == null) 
+            {
+                // Instantiate our own prefab
+                spawnedObject = Instantiate(prefab, hitPoint.position, hitPoint.rotation);
+            }
+            else
+            {
+                // If there is an existing spawnedObject, we simply move its position
+                spawnedObject.transform.SetPositionAndRotation(hitPoint.position, hitPoint.rotation);
+            }
+        } 
     }
 }
