f09805294669805ca79d95ddbf434d8119da8486
soupalter <christiandavid2401@gmail.com>
2023-06-16 18:20:09 +0800

    asdf
    asdf

---
+++ Assets/Scripts/rotat_e.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotat_e : MonoBehaviour
+{
+    public float rotatespeed = 10f;
+    private float startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startingPosition = touch.position.x;
+                    break;
+                case TouchPhase.Moved:
+                    if (startingPosition > touch.position.x)
+                    {
+                        transform.Rotate(Vector3.back, rotatespeed * Time.deltaTime);
+                    }
+                    else if (startingPosition < touch.position.x)
+                    {
+                        transform.Rotate(Vector3.back, rotatespeed * Time.deltaTime);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    Debug.Log("Touch Phase Ended.");
+                    break;
+            }
+        }
+    }
+}
